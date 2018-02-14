f71867b0d960c7883f64a9fa7aa1ea8afde553d7
Chris Gullickson <cgullickson5@gmail.com>
2018-02-14 04:36:14 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       redirect_to '/sessions/new'
     else
       session[:name] = params[:name]
-      redirect_to '/'
+      redirect_to root_path
     end
   end
 
